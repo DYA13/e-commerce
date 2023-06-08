512b220394ff56fbc1d71f10278836ac2cb8cf8f
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-06-08 11:37:18 +0200

    modified:   routes/user.Routes.js

--- routes/user.Routes.js
+++ routes/user.Routes.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const { register, login, logout } = require('../controllers/authController')
+const {
+  getAllUsers,
+  getSingleUser,
+  showCurrentUser,
+  updateUser,
+  updateUserPassword
+} = require('../controllers/userController')
 
 router.post('/register', register)
 router.post('/login', login)
