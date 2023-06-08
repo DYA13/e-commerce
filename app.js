96c0b1731b4fd2848c97ae37498aaf295e32bf04
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-06-08 10:03:15 +0200

    new file:   public/browser-app.js/index.html/main.css/ormalize.css

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-require("dotenv").config()
-require("express-async-errors")
+require('dotenv').config()
+require('express-async-errors')
 
-const express = require("express")
+const express = require('express')
 const app = express()
 
 //rest of the packages
-const morgan = require("morgan")
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
 
 //database
-const connectDB = require("./db/connect")
+const connectDB = require('./db/connect')
 
 //routers
-const authRouter = require("./routes/authRoutes")
+const authRouter = require('./routes/authRoutes')
 
 //middleware
-const notFoundMiddleware = require("./middleware/not-found")
-const errorHandlerMiddleware = require("./middleware/error-handler")
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
-app.use(morgan("tiny"))
+app.use(morgan('tiny'))
 app.use(express.json())
+app.use(cookieParser(process.env.JWT_SECRET))
+app.use(express.static('./public'))
 
-app.get("/", (req, res) => {
-  res.send("e-commerce api")
+app.get('/api/v1', (req, res) => {
+  //console.log(req.cookies)
+  console.log(req.signedCookies)
+  res.send('e-commerce api')
 })
 
-app.use("/api/v1/auth", authRouter)
+app.use('/api/v1/auth', authRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
