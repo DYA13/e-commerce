02e3459f9dadb81e8a0791e40173fa61e226d919
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-06-09 10:40:14 +0200

    new file:   utils/checkPermissions.js,modified:   controllers/authController.js,modified:   controllers/userController.js,modified:   utils/index.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,19 +28,18 @@ const login = async (req, res) => {
   if (!email || !password) {
     throw new CustomError.BadRequestError('Please provide email and password')
   }
-
   const user = await User.findOne({ email })
+
   if (!user) {
     throw new CustomError.UnauthenticatedError('Invalid Credentials')
   }
-
   const isPasswordCorrect = await user.comparePassword(password)
   if (!isPasswordCorrect) {
     throw new CustomError.UnauthenticatedError('Invalid Credentials')
   }
-
   const tokenUser = createTokenUser(user)
   attachCookiesToResponse({ res, user: tokenUser })
+
   res.status(StatusCodes.OK).json({ user: tokenUser })
 }
 
