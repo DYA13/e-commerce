40a4172029345ee4e2d96db1f1731b240ce9aba6
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-07-06 21:23:24 +0200

    modified:   controllers/authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,48 +1,63 @@
+// Import required modules and files
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
 const { attachCookiesToResponse, createTokenUser } = require('../utils')
 
+// Register user
 const register = async (req, res) => {
   const { email, name, password } = req.body
+  // Check if email already exists
   const emailAlreadyExists = await User.findOne({ email })
   if (emailAlreadyExists) {
     throw new CustomError.BadRequestError('Email already exists')
   }
 
-  //first registered user is an admin
+  // Determine user role (first registered user is an admin)
   const isFirstAccount = (await User.countDocuments({})) === 0
   const role = isFirstAccount ? 'admin' : 'user'
 
+  // Create user
   const user = await User.create({ name, email, password, role })
 
+  // Generate token for the user
   const tokenUser = createTokenUser(user)
 
+  // Attach token to response cookies
   attachCookiesToResponse({ res, user: tokenUser })
+  // Send response with created user
   res.status(StatusCodes.CREATED).json({ user: tokenUser })
 }
-
+// User login
 const login = async (req, res) => {
   const { email, password } = req.body
 
+  // Check if email and password are provided
   if (!email || !password) {
     throw new CustomError.BadRequestError('Please provide email and password')
   }
+  // Find user by email
   const user = await User.findOne({ email })
 
+  // Check if user exists
   if (!user) {
     throw new CustomError.UnauthenticatedError('Invalid Credentials')
   }
+  // Compare password
   const isPasswordCorrect = await user.comparePassword(password)
   if (!isPasswordCorrect) {
     throw new CustomError.UnauthenticatedError('Invalid Credentials')
   }
+  // Generate token for the user
   const tokenUser = createTokenUser(user)
+  // Attach token to response cookies
   attachCookiesToResponse({ res, user: tokenUser })
 
+  // Send response with logged-in user
   res.status(StatusCodes.OK).json({ user: tokenUser })
 }
 
+// User logout
 const logout = async (req, res) => {
   res.cookie('token', 'logout', {
     httpOnly: true,
@@ -51,6 +66,7 @@ const logout = async (req, res) => {
     signed: true,
     sameSite: 'none'
   })
+  // Send response
   res.status(StatusCodes.OK).json({ msg: 'user logged out!' })
 }
 
